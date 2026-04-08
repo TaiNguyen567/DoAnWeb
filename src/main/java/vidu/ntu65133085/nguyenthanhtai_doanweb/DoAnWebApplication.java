97abbd5f0f890a82ae034d51418036f885a25e7a
TaiNguyen567 <tai.nt.65cntt@ntu.edu.vn>
2026-04-08 19:59:22 +0700

    Khoi tao base code Spring Boot

---
+++ src/main/java/vidu/ntu65133085/nguyenthanhtai_doanweb/DoAnWebApplication.java
@@ -0,0 +1,13 @@
+package vidu.ntu65133085.nguyenthanhtai_doanweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoAnWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoAnWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vidu/ntu65133085/nguyenthanhtai_doanweb/DoAnWebApplicationTests.java
@@ -0,0 +1,13 @@
+package vidu.ntu65133085.nguyenthanhtai_doanweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoAnWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
