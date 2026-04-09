7878ace9f0c527600f5073bbaf35f808c23ece0e
TaiNguyen567 <tai.nt.65cntt@ntu.edu.vn>
2026-04-09 13:48:14 +0700

    Tạo HabitRepository xử lý dữ liệu bảng habits

---
+++ src/main/java/ntu/nguyenthanhtai_65133085/doanweb/repository/HabitRepository.java
@@ -0,0 +1,14 @@
+package ntu.nguyenthanhtai_65133085.doanweb.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import ntu.nguyenthanhtai_65133085.doanweb.entity.Habit;
+
+@Repository
+public interface HabitRepository extends JpaRepository<Habit, Long> {
+
+	List<Habit> findByUserId(Long userId);
+}
