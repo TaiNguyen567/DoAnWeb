380602bc543b1691a8d3ce327a1b67899e56fc2c
TaiNguyen567 <tai.nt.65cntt@ntu.edu.vn>
2026-04-10 14:17:21 +0700

    Tạo UserController cung cấp API đăng ký tài khoản

---
+++ src/main/java/ntu/nguyenthanhtai_65133085/doanweb/controller/UserController.java
@@ -0,0 +1,28 @@
+package ntu.nguyenthanhtai_65133085.doanweb.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import ntu.nguyenthanhtai_65133085.doanweb.dto.UserDTO;
+import ntu.nguyenthanhtai_65133085.doanweb.dto.UserRegisterDTO;
+import ntu.nguyenthanhtai_65133085.doanweb.service.UserService;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+	private final UserService userService;
+
+	@PostMapping("/register")
+	public ResponseEntity<UserDTO> registerUser(@Valid @RequestBody UserRegisterDTO registerDTO) {
+		UserDTO createdUser = userService.registerUser(registerDTO);
+		return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+	}
+}
