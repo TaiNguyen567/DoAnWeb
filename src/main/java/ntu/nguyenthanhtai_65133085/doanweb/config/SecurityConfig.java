2b3c1c8eefd3f0a1d45eaaf41295235bb28c3feb
TaiNguyen567 <tai.nt.65cntt@ntu.edu.vn>
2026-04-17 14:25:18 +0700

    Thêm JwtAuthenticationFilter để kiểm tra Token cho các API bảo mật

--- src/main/java/ntu/nguyenthanhtai_65133085/doanweb/config/SecurityConfig.java
+++ src/main/java/ntu/nguyenthanhtai_65133085/doanweb/config/SecurityConfig.java
@@ -5,12 +5,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
+import ntu.nguyenthanhtai_65133085.doanweb.security.JwtAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+	private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
@@ -25,8 +33,13 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(auth -> auth
-				.requestMatchers("/api/users/register", "/api/users/login").permitAll().anyRequest().authenticated());
+		http.csrf(csrf -> csrf.disable())
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.authorizeHttpRequests(auth -> auth.requestMatchers("/api/users/register", "/api/users/login")
+						.permitAll().anyRequest().authenticated());
+
+		http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
 		return http.build();
 	}
 }
