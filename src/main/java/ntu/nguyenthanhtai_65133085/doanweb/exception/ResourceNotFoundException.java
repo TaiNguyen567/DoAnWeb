47d31823fda5c537b24877ed124a8f4000b6f271
TaiNguyen567 <tai.nt.65cntt@ntu.edu.vn>
2026-05-26 18:23:39 +0700

    Create ResourceNotFoundException.java
    lỗi không tìm thấy resource

---
+++ src/main/java/ntu/nguyenthanhtai_65133085/doanweb/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package ntu.nguyenthanhtai_65133085.doanweb.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+	public ResourceNotFoundException(String message) {
+		super(message);
+	}
+}
