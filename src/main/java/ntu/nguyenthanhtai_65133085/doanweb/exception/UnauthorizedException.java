488807fdf4a3264590b01237639f1dad856d1557
TaiNguyen567 <tai.nt.65cntt@ntu.edu.vn>
2026-05-26 18:23:53 +0700

    Create UnauthorizedException.java
    lỗi không có quyền truy cập

---
+++ src/main/java/ntu/nguyenthanhtai_65133085/doanweb/exception/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package ntu.nguyenthanhtai_65133085.doanweb.exception;
+
+public class UnauthorizedException extends RuntimeException {
+	public UnauthorizedException(String message) {
+		super(message);
+	}
+}
