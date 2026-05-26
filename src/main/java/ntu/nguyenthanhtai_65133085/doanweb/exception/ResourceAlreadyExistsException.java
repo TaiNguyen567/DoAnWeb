7a663ef6d73fe3fdb0bcf2841d16f04d99e5113a
TaiNguyen567 <tai.nt.65cntt@ntu.edu.vn>
2026-05-26 18:23:26 +0700

    Create ResourceAlreadyExistsException.java
    lỗi trùng username/email

---
+++ src/main/java/ntu/nguyenthanhtai_65133085/doanweb/exception/ResourceAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package ntu.nguyenthanhtai_65133085.doanweb.exception;
+
+public class ResourceAlreadyExistsException extends RuntimeException {
+	public ResourceAlreadyExistsException(String message) {
+		super(message);
+	}
+}
