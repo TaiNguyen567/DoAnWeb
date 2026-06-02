b63fe6895c26863ac9dc01f4b00a6ef532a003fd
TaiNguyen567 <tai.nt.65cntt@ntu.edu.vn>
2026-06-02 15:42:56 +0700

    Update HabitRequestDTO.java
    Thêm tính năng chọn danh sách ngày lặp lại nhiệm vụ trong tuần

--- src/main/java/ntu/nguyenthanhtai_65133085/doanweb/dto/HabitRequestDTO.java
+++ src/main/java/ntu/nguyenthanhtai_65133085/doanweb/dto/HabitRequestDTO.java
@@ -1,5 +1,7 @@
 package ntu.nguyenthanhtai_65133085.doanweb.dto;
 
+import java.util.List;
+
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
@@ -14,4 +16,7 @@ public class HabitRequestDTO {
 
 	@Min(value = 1, message = "Mục tiêu ít nhất phải là 1")
 	private int goal;
+
+	private List<String> daysOfWeek; // Nhận List từ các checkbox trên HTML
+	private String timeFrame;
 }
