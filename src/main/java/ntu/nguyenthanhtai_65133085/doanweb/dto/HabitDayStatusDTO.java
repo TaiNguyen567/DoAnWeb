a28664e9eaaef02f4ce1c1667b5a479371d8dc1f
TaiNguyen567 <tai.nt.65cntt@ntu.edu.vn>
2026-06-02 15:41:29 +0700

    Create HabitDayStatusDTO.java
    Thêm tính năng hiển thị ngày lặp lại

---
+++ src/main/java/ntu/nguyenthanhtai_65133085/doanweb/dto/HabitDayStatusDTO.java
@@ -0,0 +1,10 @@
+package ntu.nguyenthanhtai_65133085.doanweb.dto;
+
+import lombok.Data;
+
+@Data
+public class HabitDayStatusDTO {
+	private Long habitId;
+	private String habitName;
+	private String status;
+}
